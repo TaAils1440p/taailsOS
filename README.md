f4a9e258b3ffbe2731fba216831a1dd07cfc555b
TaAils1440p <taailspro@gmail.com>
2025-06-24 16:23:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# taailsOS
+I made my own os
+
+## How to run
+Open a terminal and type:
+```bash
+make && qemu-system-i386 -fda build/main_floppy.img
+```
---
+++ src/main.asm
@@ -0,0 +1,64 @@
+org 0x7C00
+bits 16
+
+%define ENDL 0x0D, 0x0A
+
+
+start:
+    jmp main
+
+
+;
+; Prints a string to the screen.
+; Params:
+;     - ds:si points to string
+;
+puts:
+    ; Save registers we will modify
+    push si
+    push ax
+
+.loop:
+    lodsb               ; Loads next character in al
+    or al, al           ; Verify if next charcter is null?
+    jz .done
+
+    mov ah, 0x0e        ; Call bios interrupt
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop ax
+    pop si
+    ret
+
+
+
+main:
+
+    ; Setup data segment
+    mov ax, 0           ; Can't write to ds/es directly
+    mov ds, ax
+    mov es, ax
+
+    ; Setup stack
+    mov ss, ax
+    mov sp, 0x7C00      ; Stack grows downwards from where we are loaded in memory
+
+    ; Print message
+    mov si, msg_hello
+    call puts
+
+    hlt
+
+.halt:
+    jmp .halt
+
+
+
+msg_hello: db 'Hello World!', ENDL, 0
+
+
+times 510-($-$$) db 0
+dw 0AA55h
